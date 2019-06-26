35bce5a9e3ba27b42b034c769c7d509b75c38cfd
ChoWonmin <0108257@gmail.com>
2019-06-26 16:08:45 +0900

    import THREE scene

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,21 @@
         span Vuetify
         span.font-weight-light MATERIAL DESIGN
       v-spacer
-      v-btn(flat
-             href="https://github.com/vuetifyjs/vuetify/releases/latest"
-             target="_blank")
+      v-btn(flat href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank")
         .mr-2 Latest Release
     .appliacation-grid
-      v-navigation-drawer.blue.lighten-3 navigation
-      v-content.orange(orange) content
+      v-navigation-drawer.blue.lighten-3
+        v-list
+          v-list-group(prepend-icon="account_circle" value="true" v-for="item in items")
+            template(v-slot:activator)
+              v-list-tile
+                v-list-tile-title {{item.title}}
+            v-list-tile(v-for="subItem in item.list" :key="subItem.title" @click="")
+              v-list-tile-content
+                v-list-tile-title {{ subItem.title }}
+              v-list-tile-action
+                v-icon {{ subItem.action }}
+      v-content.orange(orange)
         router-view.pa-0.ma-0
 </template>
 
@@ -21,7 +29,25 @@ export default {
   name: 'App',
   data () {
     return {
-      //
+      items: [
+        {
+          title: 'Spring System', icon: 'gavel',
+          list: [
+            {
+              title: 'Basic', icon: 'gavel'
+            },
+            {
+              title: 'Collision', icon: 'gavel'
+            },
+            {
+              title: 'Contact', icon: 'gavel'
+            },
+            {
+              title: 'Cloth System', icon: 'gavel'
+            }
+          ]
+        },
+      ]
     }
   }
 }
--- src/views/springs/Springs.vue
+++ src/views/springs/Springs.vue
@@ -1,14 +1,51 @@
 <template lang="pug">
-  v-container#springs-doc
-    .text springs
-    
+  #springs-doc
 </template>
 
 <script>
+import * as THREE from 'three';
+
 export default {
-  name: 'Springs'
+  name: 'Springs',
+  mounted () {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+    var renderer = new THREE.WebGLRenderer();
+    const where = document.getElementById('springs-doc');
+    renderer.setSize(where.clientWidth, where.clientHeight);
+    where.appendChild(renderer.domElement);
+
+    var geometry = new THREE.BoxGeometry(1, 1, 1);
+    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    var cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    camera.position.z = 5;
+
+    renderer.render(scene, camera);
+
+    function animate () {
+      requestAnimationFrame(animate);
+
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+
+      renderer.render(scene, camera);
+    }
+    animate();
+  }
 }
 </script>
 
-<style lang="sass" scoped>
+<style lang="scss" scoped>
+#springs-doc {
+  width: 100%;
+  height: 100%;
+  canvas {
+    width: 100%;
+    height: 100%;
+    background-color: antiquewhite;
+  }
+}
 </style>
