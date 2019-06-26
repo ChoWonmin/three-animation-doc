fe19be785fd8d67f354c9fef3f2f0fd87e18abae
ChoWonmin <0108257@gmail.com>
2019-06-26 14:20:42 +0900

    scss

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,15 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<template lang="pug">
+  .about
+    h1 This is an about page
 </template>
+
+<script>
+export default {
+  name: 'About',
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
---
+++ src/views/springs/Springs.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  v-container#springs-doc
+    .text springs
+    
+</template>
+
+<script>
+export default {
+  name: 'Springs'
+}
+</script>
+
+<style lang="sass" scoped>
+</style>
