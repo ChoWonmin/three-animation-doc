f96908bf43e51e317a4cd17fc6983225b4ae1a91
ChoWonmin <0108257@gmail.com>
2019-07-03 20:58:35 +0900

    gravity

--- src/views/springs/Basic.vue
+++ src/views/springs/Basic.vue
@@ -26,33 +26,33 @@ export default {
     const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
     const cube = new THREE.Mesh(geometry, material);
     const floor = new THREE.Mesh(new THREE.BoxGeometry(300, 1, 300), new THREE.MeshBasicMaterial({ color: 0xf0f0f0 }));
-    cube.position.set(0, 30, 0);
+    // cube.position.set(0, 30, 0);
     floor.position.set(0, 0, 0);
     camera.position.set(0, 20, 150);
 
-    const particle = new animation3.Particle(cube, new THREE.Vector3(0, 0, 0));
+    const particle = new animation3.Particle(cube, new THREE.Vector3(0, 70, 0));
 
     scene.add(cube);
     scene.add(floor);
 
     function update () {
-      dt += 0.00001;
-
       const G = new THREE.Vector3(0, -1, 0).multiplyScalar(particle.mass * 981); // 중력가속도
+
+      dt += 0.00001;
 
       particle.addForce(G);
       particle.eval(dt);
       cube.position.set(particle.position.x, particle.position.y, particle.position.z);
 
       if (particle.position.y < floor.position.y) {
+        particle.velocity = 0;
         cube.position.setY(floor.position.y + 3);
       }
     }
 
     function animate () {
       requestAnimationFrame(animate);
-
-      // update();
+      update();
 
 
       controls.update();
